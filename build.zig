ffe71ad3f196de11e316b670eb761d4c7d5fc2c5
star-tek-mb <noob.tek.mb@gmail.com>
2023-05-01 23:55:17 +0500

    update to latest zig master

--- build.zig
+++ build.zig
@@ -15,9 +15,9 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
     exe.addModule("tray", module);
-    exe.install();
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
