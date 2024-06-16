1946bbe987cfe8fe0cbcc8c11739c55a9789296f
jinzhongjia <mail@nvimer.org>
2024-06-16 12:19:37 +0800

    Separate code file structure

--- build.zig
+++ build.zig
@@ -1,29 +1,42 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
+    // get target
     const target = b.standardTargetOptions(.{});
+    // get optimize
     const optimize = b.standardOptimizeOption(.{});
 
-    var module: *std.Build.Module = undefined;
-    switch (target.result.os.tag) {
-        .windows => {
-            module = b.addModule("tray", .{
-                .root_source_file = b.path("src/tray_windows.zig"),
-            });
-        },
-        else => {
-            @panic("Unsupported platform");
-        },
-    }
+    // export zig-tray module
+    const zigTray = switch (target.result.os.tag) {
+        .windows => b.addModule("tray", .{
+            .root_source_file = b.path("src/tray_windows.zig"),
+        }),
+        else => @panic("Unsupported platform, now only support windows"),
+    };
 
+    // build example
+    example(b, target, optimize, zigTray);
+}
+
+/// for build example
+fn example(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, module: *std.Build.Module) void {
+    if (target.result.os.tag != .windows)
+        return;
+
+    // build example
     const exe = b.addExecutable(.{
         .name = "zig-tray",
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("example/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    exe.root_module.pic = true;
+
     exe.root_module.addImport("tray", module);
+
+    // install the icon file
+    b.installFile("example/icon.ico", "bin/icon.ico");
+
+    // install artifact
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
